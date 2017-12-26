8d54fc9cdcdaeedcfa896eae9fb8d9f0cc7ac6b0
Jakub Konka <kubkon@jakubkonka.com>
2017-12-26 13:47:41 +0100

    Initial Commit

---
+++ PraktisForiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PraktisForiOS
+//
+//  Created by Jakub Konka on 26/12/2017.
+//  Copyright © 2017 Jakub Konka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
